6e61d04a7f407bae208f3f2ee67fb7305491b47f
AlanHedz <conejito.de.oro@hotmail.com>
2018-09-06 00:15:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hernandez/numeros/Bingo2Activity.java
@@ -0,0 +1,13 @@
+package com.example.hernandez.numeros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bingo2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bingo2);
+    }
+}
---
+++ app/src/main/java/com/example/hernandez/numeros/BingoActivity.java
@@ -0,0 +1,13 @@
+package com.example.hernandez.numeros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BingoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bingo);
+    }
+}
